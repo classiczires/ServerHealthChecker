a99a6edca6d3aea9af1447032e61d306cc276ca9
saeed karimi <classiczires@gmail.com>
2019-11-13 23:30:58 +0330

    fix notifications

--- lib/src/main/java/com/zires/lib/EmailNotification.kt
+++ lib/src/main/java/com/zires/lib/EmailNotification.kt
@@ -1,5 +1,9 @@
 package com.zires.lib
 
+/**
+ * Created by ClassicZires on 11/13/2019.
+ **/
+
 class EmailNotification : Notification {
     override fun notify(message: String) {
         //TODO implement send email here
--- lib/src/main/java/com/zires/lib/Notification.kt
+++ lib/src/main/java/com/zires/lib/Notification.kt
@@ -1,5 +1,9 @@
 package com.zires.lib
 
+/**
+ * Created by ClassicZires on 11/13/2019.
+ **/
+
 interface Notification {
     fun notify(message: String)
 }
